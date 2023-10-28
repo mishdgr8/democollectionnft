f0d87fe8e794bd28615bf1357e67604e9fd8c201
mishdgr8 <Mishaelmordi@gmail.com>
2023-10-28 18:25:08 +0100

    setting claim criteria

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import {
+  MediaRenderer,
+  Web3Button,
   useActiveClaimCondition,
   useActiveClaimConditionForWallet,
   useAddress,
+  useClaimIneligibilityReasons,
   useContract,
   useContractMetadata,
   useTotalCirculatingSupply,
@@ -10,6 +13,8 @@ import {
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 import { CONTRACT_ADDRESS } from "../const/addresses";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 const Home: NextPage = () => {
   const address = useAddress();
@@ -28,10 +33,87 @@ const Home: NextPage = () => {
 
   const maxClaimable = parseInt(activeClaimPhase?.maxClaimablePerWallet || "0");
 
+  const [claimQuantity, setClaimQuantity] = useState(1);
+
+  const { data: claimIneligibility, isLoading: isClaimIneleiigibilityLoading } =
+    useClaimIneligibilityReasons(contract, {
+      walletAddress: address || "",
+      quantity: 1,
+    });
+
   return (
-    <main className={styles.main}>
-      <div className={styles.container}></div>
-    </main>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        {!isContractMetadataLoading && (
+          <div className={styles.heroSection}>
+            <div>
+              <MediaRenderer
+                className={styles.imgCard}
+                src={contractMetadata?.image}
+              />
+            </div>
+            <div className={styles.writeUp}>
+              <h1 className={styles.headerCard}>{contractMetadata?.name}</h1>
+              <p>{contractMetadata?.description}</p>
+              {!isActiveClaimPhaseLoading ? (
+                <div>
+                  <p>Claim Phase: {activeClaimPhase?.metadata?.name}</p>
+                  <p>
+                    Price: {ethers.utils.formatUnits(activeClaimPhase?.price!)}
+                  </p>
+                </div>
+              ) : (
+                <p>Loading...</p>
+              )}
+              {!isTotalSupplyLoading && !isTotalClaimedLoading ? (
+                <p>
+                  Claimed: {TotalClaimed?.toNumber()} /{" "}
+                  {totalSupply?.toNumber()}
+                </p>
+              ) : (
+                <p>Loading...</p>
+              )}
+
+              {address ? (
+                !isClaimIneleiigibilityLoading ? (
+                  claimIneligibility?.length > 0 ? (
+                    claimIneligibility?.map((reason) => <p>{reason}</p>)
+                  ) : (
+                    <div>
+                      <p>
+                        You are eligible to claim
+                        {` {max claimable: ${maxClaimable})`}
+                      </p>
+                      <div className={styles.claimContainer}>
+                        <div className={styles.claimValue}>
+                          <button className={styles.claimBtn}>-</button>
+                          <input
+                            className={styles.claimInput}
+                            type="number"
+                            value={1}
+                          />
+                          <button className={styles.claimBtn}>+</button>
+                        </div>
+                        <Web3Button
+                          contractAddress={CONTRACT_ADDRESS}
+                          action={(contract) => contract.erc721.claim(1)}
+                        >
+                          Claim Nft
+                        </Web3Button>
+                      </div>
+                    </div>
+                  )
+                ) : (
+                  <p>Loading...</p>
+                )
+              ) : (
+                <p> Connect wallet to claim nft</p>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
